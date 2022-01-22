65d29336708d73600728064d3c288ea31b3fb7f5
Kay-Michael Voit <kay-michael.voit@caesar.de>
2022-01-22 19:56:28 +0100

    Fixed environment.yml to original DLC version Added headless mode (runs DLC in light mode)

--- ACM-dlcdetect/__main__.py
+++ ACM-dlcdetect/__main__.py
@@ -7,6 +7,8 @@ def main():
     # Parse inputs
     parser = argparse.ArgumentParser(description="ACM-DLCdetect - Wrapper for DLC to support label files and CCV videos used bz MPINB-BBOs ACM framework.")
     parser.add_argument('INPUT_PATH', type=str, help="Directory with job configuration")
+    parser.add_argument("--headless", action="store_true",
+                    help="Run headless")
     args = parser.parse_args()
     input_path = os.path.expanduser(args.INPUT_PATH)
 
@@ -14,6 +16,9 @@ def main():
     # TODO change config system, e.g. pass around a dictionary instead of importing the config everywhere, requiring the sys.path.insert
     sys.path.insert(0,input_path)
     print(f'Loading {input_path} ...')
+
+    if args.headless:
+        os.environ['DLClight'] = 'True'
 
     from . import main
     from . import save
